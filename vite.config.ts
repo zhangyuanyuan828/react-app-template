59af79f8fa8f8bea7be3672a920efc27a80db6a6
玲玲子 <zhangyuanyuan828@outlook.com>
2023-06-22 09:11:39 +0800

    chore: update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { resolve } from 'node:path'
+import { promisify } from 'node:util'
+import { brotliCompress } from 'node:zlib'
+import compress from 'rollup-plugin-gzip'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src')
+    }
+  },
+  plugins: [
+    react(),
+    compress({
+      filter: /\.(js|mjs|cjs|json|css|html|xml|svg|wasm)$/i
+    }),
+    compress({
+      filter: /\.(js|mjs|cjs|json|css|html|xml|svg|wasm)$/i,
+      fileName: '.br',
+      customCompression: (content) => promisify(brotliCompress)(content)
+    })
+  ]
 })
