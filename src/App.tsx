ca2db7e37ad5bcb010f299c700d24e3d86f66e99
玲玲子 <zhangyuanyuan828@outlook.com>
2023-12-08 09:53:38 +0800

    chore: remove mobx

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,14 @@
-import { Box, Button, Code, Flex, Text, Title } from '@mantine/core'
-import { observer } from 'mobx-react-lite'
+import { Button, Center } from '@mantine/core'
+import { useState } from 'react'
 
-import reactLogo from './assets/react.svg'
-import { useStores } from './stores'
-
-export const App = observer(() => {
-  const { appStore } = useStores()
+export function App() {
+  const [count, setCount] = useState<number>(1)
 
   return (
-    <Flex w="100vw" h="100vh" direction="column" gap="md" justify="center" align="center" p="md">
-      <Flex gap="md">
-        <Box component="a" href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <Box component="img" h="8em" p="md" src="/vite.svg" alt="Vite logo" />
-        </Box>
-        <Box component="a" href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <Box component="img" h="8em" p="md" src={reactLogo} alt="React logo" />
-        </Box>
-      </Flex>
-      <Title order={1}>Vite + React</Title>
-      <Button variant="outline" onClick={() => appStore.increase()}>
-        count is {appStore.count}
-      </Button>
-      <Text>
-        Edit <Code>src/App.tsx</Code> and save to test HMR
-      </Text>
-      <Text c="gray">Click on the Vite and React logos to learn more</Text>
-    </Flex>
+    <Center w="100vw" h="100vh">
+      <Button onClick={() => setCount(count + 1)}>{'Count: ' + count}</Button>
+    </Center>
   )
-})
+}
 
 export default App
